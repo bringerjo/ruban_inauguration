89f63e7bba3d16099ef0da25076829d93e5988b8
bringerjo <bringer.jo@gmail.com>
2018-09-28 15:26:11 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(".ribbon-middle-1").click(function(){
+	//$('.ribbon-middle-1').css({"-webkit-transform":"translate(100px,100px)"});​
+	$('.ribbon-middle-1').css({"-webkit-transform":"translate(-400px,0px)"})
+	console.log("clic");
+});
+
+$(".ribbon-middle-2").click(function(){
+	//$('.ribbon-middle-1').css({"-webkit-transform":"translate(100px,100px)"});​
+	$('.ribbon-middle-1').css({"-webkit-transform":"translate(-400px,0px)"})
+	console.log("clic");
+});
+
+$(".ribbon-left").click(function(){
+	//$('.ribbon-middle-1').css({"-webkit-transform":"translate(100px,100px)"});​
+	$('.ribbon-middle-1').css({"-webkit-transform":"translate(-400px,0px)"})
+	console.log("clic");
+});
+
+$(".right").click(function(){
+	//$('.ribbon-middle-1').css({"-webkit-transform":"translate(100px,100px)"});​
+	$('.ribbon-middle-1').css({"-webkit-transform":"translate(-400px,0px)"})
+	console.log("clic");
+});
